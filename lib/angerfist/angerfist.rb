7e6c09855e3e76aa5f0241d116de2ed8b594c3db
pikesley <github@orgraphone.org>
2015-04-22 14:42:09 +0100

    Passing test

--- lib/angerfist/angerfist.rb
+++ lib/angerfist/angerfist.rb
@@ -2,13 +2,19 @@ module Rack
   class Angerfist
     def initialize app, config
       @app = app
+      @gabba = Gabba::Gabba.new(config[:tracker_id], config[:domain])
     end
 
     def call env
       @env = env
       status, headers, response = @app.call(@env)
+      @gabba.page_view(full_path, full_path)
 
       [ status, headers, response ]
+    end
+
+    def full_path
+      (@env['PATH_INFO'] + '?' + @env['QUERY_STRING']).chomp('?')
     end
   end
 end
